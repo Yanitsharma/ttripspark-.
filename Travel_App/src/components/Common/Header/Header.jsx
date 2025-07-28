920f56d5c17e7a4161a609a27551ca45188314e9
Vijay Kumar <vijaykumar20cy@gmail.com>
2025-07-29 00:26:03 +0530

    first

--- Travel_App/src/components/Common/Header/Header.jsx
+++ Travel_App/src/components/Common/Header/Header.jsx
@@ -1,110 +1,132 @@
 import React, { useEffect, useState } from "react";
 import {
   Container,
-  Row,
   Navbar,
   Offcanvas,
   Nav,
   NavDropdown,
 } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import "../Header/header.css";
 
 const Header = () => {
   const [open, setOpen] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
 
   const toggleMenu = () => {
     setOpen(!open);
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     window.addEventListener("scroll", isSticky);
-    return ()=>{
-      window.removeEventListener("scroll", isSticky)
-    }
-  })
+    return () => {
+      window.removeEventListener("scroll", isSticky);
+    };
+  }, []);
 
-  // sticky Header 
-  const isSticky=(e)=>{
-    const header = document.querySelector('.header-section');
+  // Sticky Header
+  const isSticky = () => {
+    const header = document.querySelector(".header-section");
     const scrollTop = window.scrollY;
-    scrollTop >= 120 ? header.classList.add('is-sticky') :
-    header.classList.remove('is-sticky')
-  }
+    scrollTop >= 120
+      ? header.classList.add("is-sticky")
+      : header.classList.remove("is-sticky");
+  };
 
-
- 
+  // Scroll to gallery
+  const handleGalleryClick = () => {
+    setOpen(false); // close offcanvas if open
+    if (location.pathname !== "/") {
+      navigate("/");
+      setTimeout(() => {
+        const galleryEl = document.getElementById("gallery");
+        if (galleryEl) {
+          galleryEl.scrollIntoView({ behavior: "smooth" });
+        }
+      }, 300); // wait for navigation and Home component to render
+    } else {
+      const galleryEl = document.getElementById("gallery");
+      if (galleryEl) {
+        galleryEl.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  };
 
   return (
-    
     <header className="header-section">
       <Container>
-       
-          <Navbar expand="lg" className="p-0">
-            {/* Logo Section  */}
-            <Navbar.Brand>
-              <NavLink to="/">TripSpark</NavLink>
-            </Navbar.Brand>
-            {/* End Logo Section  */}
+        <Navbar expand="lg" className="p-0">
+          {/* Logo */}
+          <Navbar.Brand>
+            <NavLink to="/">TripSpark</NavLink>
+          </Navbar.Brand>
 
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-lg`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
-              placement="start"
-              show={open}
-            >
-              {/*mobile Logo Section  */}
-              <Offcanvas.Header>
-                <h1 className="logo">TripSpark</h1>
-                <span className="navbar-toggler ms-auto"  onClick={toggleMenu}>
-                  <i className="bi bi-x-lg"></i>
-                </span>
-              </Offcanvas.Header>
-              {/*end mobile Logo Section  */}
+          {/* Offcanvas for Mobile */}
+          <Navbar.Offcanvas
+            id="offcanvasNavbar-expand-lg"
+            aria-labelledby="offcanvasNavbarLabel-expand-lg"
+            placement="start"
+            show={open}
+          >
+            <Offcanvas.Header>
+              <h1 className="logo">TripSpark</h1>
+              <span className="navbar-toggler ms-auto" onClick={toggleMenu}>
+                <i className="bi bi-x-lg"></i>
+              </span>
+            </Offcanvas.Header>
 
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <NavLink className="nav-link" to="/" >
-                    Home 
-                  </NavLink>
-                  <NavLink className="nav-link" to="/" >
-                    ABOUT US
-                  </NavLink>
-                  <NavLink className="nav-link" to="/" >
-                    TOURS
-                  </NavLink>
-
-                  <NavDropdown
-                    title="DESTINATION"
-                    id={`offcanvasNavbarDropdown-expand-lg`}
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <NavLink className="nav-link" to="/" onClick={toggleMenu}>
+                  Home
+                </NavLink>
+                <NavLink className="nav-link" to="/" onClick={toggleMenu}>
+                  ABOUT US
+                </NavLink>
+                <NavLink className="nav-link" to="/" onClick={toggleMenu}>
+                  TOURS
+                </NavLink>
+                <NavDropdown
+                  title="DESTINATION"
+                  id="offcanvasNavbarDropdown-expand-lg"
+                >
+                  <NavLink
+                    className="nav-link text-dark"
+                    to="/"
+                    onClick={toggleMenu}
                   >
-                   
-                      
-                    <NavLink className="nav-link text-dark" to="/" >
                     SPAIN TOURS
                   </NavLink>
-                  
-                   
-                  </NavDropdown>
-                  <NavLink className="nav-link" to="/" >
-                    GALLERY
-                  </NavLink>
-                  <NavLink className="nav-link" to="/" >
-                    CONTACT
-                  </NavLink>
-                </Nav>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-            <div className="ms-md-4 ms-2">
-              <NavLink className="primaryBtn d-none d-sm-inline-block">
-                Book Now
-              </NavLink>
-              <li className="d-inline-block d-lg-none ms-3 toggle_btn">
-                <i className={open ? "bi bi-x-lg" : "bi bi-list"}  onClick={toggleMenu}></i>
-              </li>
-            </div>
-          </Navbar>
-    
+                </NavDropdown>
+
+                <span
+                  className="nav-link gallery-link"
+                  role="button"
+                  onClick={handleGalleryClick}
+                >
+                  GALLERY
+                </span>
+
+                <NavLink className="nav-link" to="/" onClick={toggleMenu}>
+                  CONTACT
+                </NavLink>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+
+          <div className="ms-md-4 ms-2">
+            <NavLink className="primaryBtn d-none d-sm-inline-block">
+              Book Now
+            </NavLink>
+            <li className="d-inline-block d-lg-none ms-3 toggle_btn">
+              <i
+                className={open ? "bi bi-x-lg" : "bi bi-list"}
+                onClick={toggleMenu}
+              ></i>
+            </li>
+          </div>
+        </Navbar>
       </Container>
     </header>
   );
--- Travel_App/src/pages/Home/Home.jsx
+++ Travel_App/src/pages/Home/Home.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import Banner from "../../components/Banner/Banner";
 import AdvanceSearch from "../../components/AdvanceSearch/AdvanceSearch";
 import Features from "../../components/Features/Features";
-import { Container, Row, Col,  } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import Gallery from "../../components/Gallery/Gallery";
 
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -10,21 +11,17 @@ import "slick-carousel/slick/slick-theme.css";
 
 import "./home.css";
 
-
-import Gallery from "../../components/Gallery/Gallery";
 import Cards from "../../components/Cards/Cards";
 import { destinationsData, popularsData } from "../../utils/data";
 import PopularCard from "../../components/Cards/PopularCard";
 
-
 const Home = () => {
-  var settings = {
+  const settings = {
     dots: false,
     infinite: true,
     autoplay: true,
     slidesToShow: 4,
     slidesToScroll: 1,
-
     responsive: [
       {
         breakpoint: 1024,
@@ -67,16 +64,13 @@ const Home = () => {
     ],
   };
 
-
-
   return (
     <>
       <Banner />
       <AdvanceSearch />
       <Features />
 
-      {/* tour seciton start */}
-
+      {/* Tours Section */}
       <section className="tours_section slick_slider">
         <Container>
           <Row>
@@ -90,62 +84,53 @@ const Home = () => {
           <Row>
             <Col md="12">
               <Slider {...settings}>
-                {destinationsData.map((destination, inx) => {
-                  return (
-                    <Cards destination={destination} key={inx} />
-                  );
-                })}
+                {destinationsData.map((destination, index) => (
+                  <Cards destination={destination} key={index} />
+                ))}
               </Slider>
             </Col>
           </Row>
         </Container>
       </section>
 
-      {/* tour seciton start */}
-
+      {/* Popular Section */}
       <section className="popular py-5">
-      <Container>
-        <Row>
-          <Col md="12">
-            <div className="main_heading">
-              <h1> Popular Activities </h1>
-            </div>
-          </Col>
-        </Row>
-        <Row>
-        {popularsData.map((val, inx)=>{
-          return(
-          <Col  md={3} sm={6} xs={12} className="mb-5" key={inx}>
-            <PopularCard val={val} />
-          </Col>
-        )
-        })}
-        </Row>
-      </Container>
-    </section>
+        <Container>
+          <Row>
+            <Col md="12">
+              <div className="main_heading">
+                <h1> Popular Activities </h1>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            {popularsData.map((val, index) => (
+              <Col md={3} sm={6} xs={12} className="mb-5" key={index}>
+                <PopularCard val={val} />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </section>
 
+      {/* Call to Action Section */}
       <section className="call_us">
         <Container>
           <Row className="align-items-center">
             <Col md="8">
               <h5 className="title">CALL TO ACTION</h5>
               <h2 className="heading">
-                READY FOR UNFORGATABLE TRAVEL. REMEMBER US!
+                READY FOR UNFORGETTABLE TRAVEL. REMEMBER US!
               </h2>
               <p className="text">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s,{" "}
+                text ever since the 1500s.
               </p>
             </Col>
             <Col md="4" className="text-center mt-3 mt-md-0">
-              <a
-                href="tel:6398312365"
-                className="secondary_btn bounce"
-                rel="no"
-              >
-                {" "}
-                Contact Us !
+              <a href="tel:6398312365" className="secondary_btn bounce">
+                Contact Us!
               </a>
             </Col>
           </Row>
@@ -153,12 +138,12 @@ const Home = () => {
         <div className="overlay"></div>
       </section>
 
-      <section className="gallery">
+      <section className="gallery" id="gallery">
         <Container>
           <Row>
             <Col md="12">
               <div className="main_heading">
-                <h1>Photo Gallery </h1>
+                <h1>Photo Gallery</h1>
               </div>
             </Col>
           </Row>
